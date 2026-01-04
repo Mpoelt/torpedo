1de38137c8873cdd9fa78c8ef9992ddd664f3630
Molnár Péter <ranlom0503@gmail.com>
2026-01-04 15:01:27 +0100

    made a MapInitDeciderService construktor with consoleService, consoleMapInit and fileMapInit.

--- src/main/java/org/example/service/MapInitDeciderService.java
+++ src/main/java/org/example/service/MapInitDeciderService.java
@@ -3,18 +3,21 @@ package org.example.service;
 import org.example.init.ConsoleMapInit;
 import org.example.init.FileMapInit;
 import org.example.init.MapInit;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MapInitDeciderService {
 
-    @Autowired
-    private ConsoleService consoleService;
-    @Autowired
-    private ConsoleMapInit consoleMapInit;
-    @Autowired
-    private FileMapInit fileMapInit;
+    private final ConsoleService consoleService;
+    private final ConsoleMapInit consoleMapInit;
+    private final FileMapInit fileMapInit;
+
+    public MapInitDeciderService(final ConsoleService consoleService, final ConsoleMapInit consoleMapInit,
+                                 final FileMapInit fileMapInit){
+        this.consoleService = consoleService;
+        this.consoleMapInit = consoleMapInit;
+        this.fileMapInit = fileMapInit;
+    }
 
     public MapInit getMapInitInstance(){
         int option = consoleService.readIntFromConsole("Please provide number '1' for manual setup or '2' for loading from a file");
